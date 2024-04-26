5d25ead7507bb8b5505e463340ea84c0eee95d0e
Ink81100 <129943973+Ink81100@users.noreply.github.com>
2024-04-26 21:56:35 +0200

    add form validity

---
+++ script/formSubmit.js
@@ -0,0 +1,20 @@
+// Get the form element
+const form = document.getElementById('myForm');
+
+// Get the element that triggers form submission
+const submitElement = document.querySelector('.submit');
+
+// Attach a click event listener to the submitElement
+submitElement.addEventListener('click', function(event) {
+    // Prevent the default action of the click event
+    event.preventDefault();
+
+    // Check if the form is valid
+    if (form.checkValidity()) {
+      // If the form is valid, submit it programmatically
+      form.submit();
+    } else {
+      // If the form is not valid, display the browser's default validation messages
+      form.reportValidity();
+    }
+ });
